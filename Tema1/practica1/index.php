2a409e8656ae721ff40f49ec634a16ab1c07ff46
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2023-09-19 10:06:08 +0200

    recogida de datos

--- Tema1/practica1/index.php
+++ Tema1/practica1/index.php
@@ -9,7 +9,7 @@
 
 <body>
     <h1>Rellena tu CV</h1>
-    <form action="index.php" method="post" enctype="multipart/form-data">
+    <form action="recogida.php" method="get" enctype="multipart/form-data">
         <p><label for="nombre">Nombre:</label></br>
             <input type="text" name="nombre" id="nombre">
             </br>
