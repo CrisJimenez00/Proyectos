15ec5f2f7bd7493f8a4fc781310c0b77dcd1ac56
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2023-09-25 09:55:21 +0200

    Ejercicio acabado

--- Tema1/ejercicio1CristinaJ/vistas/vista_datos.php
+++ Tema1/ejercicio1CristinaJ/vistas/vista_datos.php
@@ -10,18 +10,18 @@
 <body>
     <h1>Datos recogidos</h1>
     <?php
-        echo "<p><strong>Nombre: </strong>" . $_POST["nombre"] . "</p>";
-        echo "<p><strong>Nacimiento: </strong>" . $_POST["nacimiento"] . "</p>";
-        //Con checkbox y radio si no existe peta el programa, se tiene que usar isset si o si
-        if (isset($_POST["sexo"])) {
-            echo "<p><strong>Sexo: </strong>" . $_POST["sexo"] . "</p>";
-        } else {
-            echo "<p><strong>Sexo: </strong> No seleccionado</p>";
-        }
-        //Forma simple, no sé recorrer aficiones[] para que me diga lo que contiene
-        echo "<p><strong>Aficiones: </strong>";
-        //la forma más básica
-        /*if (isset($_POST["deportes"])) {
+    echo "<p><strong>Nombre: </strong>" . $_POST["nombre"] . "</p>";
+    echo "<p><strong>Nacimiento: </strong>" . $_POST["nacimiento"] . "</p>";
+    //Con checkbox y radio si no existe peta el programa, se tiene que usar isset si o si
+    if (isset($_POST["sexo"])) {
+        echo "<p><strong>Sexo: </strong>" . $_POST["sexo"] . "</p>";
+    } else {
+        echo "<p><strong>Sexo: </strong> No seleccionado</p>";
+    }
+    //Forma simple, no sé recorrer aficiones[] para que me diga lo que contiene
+    
+    //la forma más básica
+    /*if (isset($_POST["deportes"])) {
             echo $_POST["deportes"] . ",";
         } 
         if (isset($_POST["lectura"])) {
@@ -30,31 +30,44 @@
          if (isset($_POST["otros"])) {
             echo $_POST["otros"] ;
         }*/
-        if (isset($_POST["deportes"]) || isset($_POST["lectura"]) || isset($_POST["otros"])) {
+    if (!isset($_POST["aficiones"])) {
+        echo "<p><b>No has seleccionado ninguna afición</b></p>";
+    } else if (count($_POST["aficiones"]) == 1) {
+        echo "<p><b>La afición seleccionada ha sido: </b></p>";
+        echo "<ol>";
+        echo "<li>" . $_POST["aficiones"][0] . "</li>";
+        echo "</ol>";
+    } else {
+        echo "<p><b>Las aficiones seleccionadas han sido: </b></p>";
+        echo "<ol>";
+        for ($i = 0; $i < count($_POST["aficiones"]); $i++) {
+            echo "<li>" . $_POST["aficiones"][$i] . "</li>";
+        }
+        echo "</ol>";
+    }
+    /*if (isset($_POST["Deportes"]) || isset($_POST["Lectura"]) || isset($_POST["Otros"])) {
             echo "<ol>";
 
-            if (isset($_POST["deportes"])) {
+            if (isset($_POST["Deportes"])) {
                 echo "<li>Deportes</li>";
             }
-            if (isset($_POST["lectura"])) {
+            if (isset($_POST["Lectura"])) {
                 echo "<li>Lectura</li>";
             }
-            if (isset($_POST["otros"])) {
+            if (isset($_POST["Otros"])) {
                 echo "<li>Otros</li>";
             }
 
             echo "</ol>";
         } else {
             echo "<p><b>No has seleccionado ninguna afición</b></p>";
-        }
-        echo "</p>";
-        if($_POST["comentarios"]!= ""){
-            echo "<p><strong>Comentarios: </strong>" . $_POST["comentarios"] . "</p>";
-        }else{
-            echo "<p><strong>No ha hecho ningún comentario </strong></p>";
-        }
-        
-    
+        }*/
+    echo "</p>";
+    if ($_POST["comentarios"] != "") {
+        echo "<p><strong>El comentario ha sido: </strong>" . $_POST["comentarios"] . "</p>";
+    } else {
+        echo "<p><strong>No ha hecho ningún comentario </strong></p>";
+    }
     ?>
 </body>
 
--- Tema1/ejercicio1CristinaJ/vistas/vista_fomulario.php
+++ Tema1/ejercicio1CristinaJ/vistas/vista_fomulario.php
@@ -1,3 +1,17 @@
+<?php
+//Hacemos a mano la funcion in_array($valor, $array)
+function en_array($valor, $arr)
+{
+    $esta = false;
+    for ($i = 0; $i < count($arr); $i++) {
+        if ($arr[$i] == $valor) {
+            $esta = true;
+            break;
+        }
+    }
+    return $esta;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -31,9 +45,21 @@
 
         <p>Nacido en:
             <select name="nacimiento" id="nacimiento">
-                <option value="Málaga">Málaga</option>
-                <option value="Sevilla">Sevilla</option>
-                <option value="Cádiz" selected>Cádiz</option>
+                <option value="Málaga" <?php
+                                        if (isset($_POST["nacimiento"]) && $_POST["nacimiento"] == "Málaga") {
+                                            echo "selected";
+                                        }
+                                        ?>>Málaga</option>
+                <option value="Sevilla" <?php
+                                        if (isset($_POST["nacimiento"]) && $_POST["nacimiento"] == "Sevilla") {
+                                            echo "selected";
+                                        }
+                                        ?>>Sevilla</option>
+                <option value="Cádiz" <?php
+                                        if (!isset($_POST["nacimiento"]) || (isset($_POST["nacimiento"]) && $_POST["nacimiento"] == "Cádiz")) {
+                                            echo "selected";
+                                        }
+                                        ?>>Cádiz</option>
             </select>
         </p>
         <p>Sexo
@@ -44,18 +70,45 @@
             ?>
             </br>
 
-            <input type="radio" name="sexo" id="hombre" value="Hombre" />
+            <input type="radio" <?php
+                                if (isset($_POST["sexo"]) && $_POST["sexo"] == "Hombre") {
+                                    echo "checked";
+                                }
+                                ?> name="sexo" id="hombre" value="Hombre" />
             <label for="hombre">Hombre</label></br>
-            <input type="radio" name="sexo" id="mujer" value="Mujer" />
+            <input <?php
+                    if (isset($_POST["sexo"]) && $_POST["sexo"] == "Mujer") {
+                        echo "checked";
+                    }
+                    ?> type="radio" name="sexo" id="mujer" value="Mujer" />
             <label for="mujer">Mujer</label>
         </p>
         <p>Aficiones:
-            <label for="deportes">Deportes</label>
+            <!--<label for="deportes">Deportes</label>
             <input type="checkbox" name="deportes" id="deportes" value="deportes">
             <label for="lectura">Lectura</label>
             <input type="checkbox" name="lectura" id="lectura" value="lectura">
             <label for="otros">Otros</label>
-            <input type="checkbox" name="otros" id="otros" value="otros">
+            <input type="checkbox" name="otros" id="otros" value="otros">-->
+            <!--Corrección-->
+            <label for="deportes">Deportes</label>
+            <input type="checkbox" name="aficiones[]" id="deportes" value="Deportes" <?php
+                                                                                        if (isset($_POST["aficiones"]) && en_array("Deportes", $_POST["aficiones"])) {
+                                                                                            echo "checked";
+                                                                                        }
+                                                                                        ?>>
+            <label for="lectura">Lectura</label>
+            <input type="checkbox" name="aficiones[]" id="lectura" value="Lectura" <?php
+                                                                                    if (isset($_POST["aficiones"]) && en_array("Lectura", $_POST["aficiones"])) {
+                                                                                        echo "checked";
+                                                                                    }
+                                                                                    ?>>
+            <label for="otros">Otros</label>
+            <input type="checkbox" name="aficiones[]" id="otros" value="Otros" <?php
+                                                                                if (isset($_POST["aficiones"]) && en_array("Otros", $_POST["aficiones"])) {
+                                                                                    echo "checked";
+                                                                                }
+                                                                                ?>>
         </p>
         <p>
             <label for="area">Comentarios:</label>
