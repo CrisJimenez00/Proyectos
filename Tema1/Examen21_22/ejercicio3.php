9e14c3c3d267d2c76983a9e70320e88e1c1be042
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2023-10-24 10:12:35 +0200

    hay que acabar el 4

--- Tema1/Examen21_22/ejercicio3.php
+++ Tema1/Examen21_22/ejercicio3.php
@@ -2,22 +2,52 @@
 if (isset($_POST["btnSeparar"])) {
     $error_formulario = $_POST["texto"] == "";
 }
-//Me cuenta las letras, no las palabras
-function mi_explode($texto, $separador)
+function contador($texto)
 {
     $contador = 0;
-    $contador2=0;
-    $array_palabras = [];
+    //Mientras exista la posición de $texto
     while (isset($texto[$contador])) {
-        if ($texto[$contador] == $separador) {
-            $contador++;
-        } else {
-            $array_palabras[$contador2] .= $texto[$contador];
-            $contador++;
-            $contador2++;
+        $contador++;
+    }
+    return $contador;
+}
+function mi_explode($texto,$sep){
+    $aux = [];
+    $l_texto = contador($texto);
+    $i = 0;
+
+    // QUITAR SEPARADORES DEL PRINCIPIO ,,,,
+    while ($i<$l_texto && $texto[$i]==$sep) {
+        $i++;
+    }
+    // YA QUITADOS LOS DE ALANTE:
+    if($i<$l_texto){
+        $j = 0;
+        // METE LA PRIMERA LETRA
+        $aux[$j] = $texto[$i];
+        // HASTA EL FINAL
+        for ($i=$i+1; $i<$l_texto ; $i++) { 
+            // SI NO ES SEPARADOR LO METE EN AUX
+            if($texto[$i]!=$sep){
+                $aux[$j] .=$texto[$i];
+            }else{
+                // HE ENCONTRADO UN SEPARADPOR
+                // LOS QUITO OTRA VEZ
+                while ($i<$l_texto && $texto[$i]==$sep) {
+                    $i++;
+                }
+                // PARA LOS QUE SE REPITEN AL FINAL
+                if($i<$l_texto){
+                    $j++;
+                    $aux[$j]=$texto[$i];
+                }
+            }
         }
     }
-    return $array_palabras;
+
+    
+
+    return $aux;
 }
 ?>
 <!DOCTYPE html>
@@ -31,26 +61,47 @@ function mi_explode($texto, $separador)
 
 <body>
     <form action="ejercicio3.php" method="post">
-        <p> <select name="separador" id="separador">
-                <option value=".">.(punto)</option>
-                <option value=",">,(coma)</option>
-                <option value=";">;(punto y coma)</option>
-                <option value=":">:(dos puntos)</option>
-                <option value=" "> (espacio)</option>
+        <p>
+            Elija separador: <select name="separador" id="separador">
+                <option <?php if (isset($_POST["btnSeparar"]) && $_POST["separador"] == ".") {
+                            echo "selected";
+                        } ?> value=".">.(punto)</option>
+                <option <?php if (isset($_POST["btnSeparar"]) && $_POST["separador"] == ",") {
+                            echo "selected";
+                        } ?> value=",">,(coma)</option>
+                <option <?php if (isset($_POST["btnSeparar"]) && $_POST["separador"] == ";") {
+                            echo "selected";
+                        } ?> value=";">;(punto y coma)</option>
+                <option <?php if (isset($_POST["btnSeparar"]) && $_POST["separador"] == ":") {
+                            echo "selected";
+                        } ?> value=":">:(dos puntos)</option>
+                <option <?php if (isset($_POST["btnSeparar"]) && $_POST["separador"] == " ") {
+                            echo "selected";
+                        } ?> value=" "> (espacio)</option>
             </select>
+        </p>
+        <p>
             <label for="texto">Inserte un texto</label>
             <input type="text" name="texto" id="texto" value="<?php
                                                                 if (isset($_POST["texto"])) {
                                                                     echo $_POST["texto"];
                                                                 }
                                                                 ?>">
+            <?php
+            if (isset($_POST["btnSeparar"])) {
+                if ($_POST["texto"] == "") {
+                    echo "<span>*Campo vacío*</span>";
+                }
+            }
+            ?>
         </p>
+
         <button type="submit" name="btnSeparar">Separar</button>
     </form>
     <?php
     if (isset($_POST["btnSeparar"]) && !$error_formulario) {
-
-        echo "<p>Tiene una longitud de: " . count(mi_explode($_POST["texto"], $_POST["separador"])) . " palabras</p>";
+        echo "<p>El número de palabras diferenciadas por " . $_POST["separador"] . " es de " . count(mi_explode($_POST["texto"], $_POST["separador"])) . "</p>";
+       
     }
     ?>
 </body>
---
+++ Tema1/Examen21_22/ejercicio4.php
@@ -0,0 +1,161 @@
+<?php
+if (isset($_POST["enviar"])) {
+
+    // NO SE HA SELECCIONADO ARCHIVO
+    $errorVacio = $_FILES["fichero"]["name"] == "";
+
+    // SI TIENE UN ERROR DE ARCHIVO
+    $errorArchivo = $_FILES["fichero"]["error"];
+
+    // SI NO ES TXT
+    $errorFormato = $_FILES["fichero"]["type"] != "text/plain";
+
+    // SI SUPERA EL TAMAÑO
+    $errorTaman = $_FILES["fichero"]["size"] > 1000 * 1024;
+
+    $errorForm = $errorVacio || $errorArchivo ||  $errorFormato || $errorTaman;
+}
+
+function mi_strlen($texto){
+    $cont=0;
+    while (isset($texto[$cont])){
+        $cont++;
+    }
+    return $cont;
+}
+
+function mi_explode($sep,$texto){
+    $aux = [];
+
+    $l_texto = mi_strlen($texto);
+
+    $i = 0;
+
+    // QUITAR SEPARADORES DEL PRINCIPIO ,,,,
+    while ($i<$l_texto && $texto[$i]==$sep) {
+        $i++;
+    }
+    // YA QUITADOS LOS DE ALANTE:
+    if($i<$l_texto){
+        $j = 0;
+        // METE LA PRIMERA LETRA
+        $aux[$j] = $texto[$i];
+        // HASTA EL FINAL
+        for ($i=$i+1; $i<$l_texto ; $i++) { 
+            // SI NO ES SEPARADOR LO METE EN AUX
+            if($texto[$i]!=$sep){
+                $aux[$j] .=$texto[$i];
+            }else{
+                // HE ENCONTRADO UN SEPARADPOR
+                // LOS QUITO OTRA VEZ
+                while ($i<$l_texto && $texto[$i]==$sep) {
+                    $i++;
+                }
+                // PARA LOS QUE SE REPITEN AL FINAL
+                if($i<$l_texto){
+                    $j++;
+                    $aux[$j]=$texto[$i];
+                }
+            }
+        }
+    }
+    return $aux;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 4</title>
+    <style>
+        .error{
+            color: red;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Ejercicio 4</h1>
+    <?php
+
+    if(isset($_POST["enviar"]) && !$errorForm){
+        @$var = move_uploaded_file($_FILES["fichero"]["tmp_name"], "Horario/horarios.txt");
+        if(!$var){
+            echo "<h3>No se ha podido mover a la carpeta destino</h3>";
+        }
+    }
+
+    @$fd =fopen("Horario/horarios.txt","r");
+
+    if($fd){
+        echo "<h2>Horario de los profesores</h2>";
+
+        $options ="";
+        while($linea = fgets($fd)){
+            // GUARDO LOS DE LA PRIMERA COLUMNA
+            $datos_linea = mi_explode("\t",$linea);
+
+            $profesores[]=$datos_linea[0];
+
+            if(isset($_POST["enviar"]) && $_POST["profesores"] == $datos_linea[0]){
+                $options.= "<option selected value='".$datos_linea[0]."'>".$datos_linea[0]."</option>";
+                $datos_prof_selec=$datos_linea;
+            }else{
+                $options.= "<option value='".$datos_linea[0]."'>".$datos_linea[0]."</option>";
+            }
+            
+        }
+        fclose($fd);
+        ?>
+        <form action="ejercicio4.php" method="post">
+
+        <p>
+            <label for="profesor">Horario del profesor</label>
+            <select name="profesor" id="profesor">
+                <?php
+                echo $options;
+                ?>
+            </select>
+            <button name="enviarHorario" type="submit">Ver horario</button>
+        </p>
+        </form>
+
+
+        <?php
+        if (isset($_POST["horario"])) {
+            # code...
+        }
+        
+    }else{
+        ?>
+        <h2>No se encuentra el archivo <em>Horario/horarios.txt</em></h2>
+        <form action="ejercicio4.php" method="POST" enctype="multipart/form-data">
+        <p>
+            <label for="fich">Seleccione un archivo de texto (MAX. 1MB)</label>
+            <input type="file" name="fichero" id="fich">
+            <?php
+            if (isset($_POST["enviar"]) && $errorForm) {
+                if ($errorVacio) {
+                    echo "<span class=error>**</span>";
+                } else if ($errorArchivo) {
+                    echo "<span class=error>* No se ha podido subir el archivo *</span>";
+                } else if ($errorFormato) {
+                    echo "<span class=error>* El formato debe ser .txt *</span>";
+                } else {
+                    echo "<span class=error>* El tamaño es superior *</span>";
+                }
+            }
+            ?>
+        </p>
+        <button type="submit" name="enviar">Enviar</button>
+
+    </form>
+        <?php
+    }
+    ?>
+</body>
+
+</html>
