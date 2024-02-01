c001f6c166f7fece907fb80d68ac7db26b95446e
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2024-02-01 14:28:30 +0100

    Ejercicio 4 con seguridad (Ejercicio 5 hecho)

--- Tema3/Servicios_Web/Ejercicio345/Ejercicio3/login_restful/index.php
+++ Tema3/Servicios_Web/Ejercicio345/Ejercicio3/login_restful/index.php
@@ -4,76 +4,151 @@ require "src/funciones_ctes.php";
 
 require __DIR__ . '/Slim/autoload.php';
 
-$app= new \Slim\App;
+$app = new \Slim\App;
 
 
-$app->get('/usuarios',function(){
+$app->get('/usuarios', function () {
 
     echo json_encode(obtener_usuarios());
 });
-
-$app->get('/usuario/{id_usuario}',function($request){
-
-    echo json_encode(obtener_usuario($request->getAttribute("id_usuario")));
+$app->get('/usuarios', function ($request) {
+    $api_key = $request->gestParam("api_key");
+    session_id($api_key);
+    session_start();
+    if (isset($_SESSION["usuario"]) && $_SESSION["tipo"] == "admin") {
+        echo json_encode(obtener_usuarios());
+    } else {
+        session_destroy();
+        echo json_encode(array("no_login" => "No tienes permisos para usar este servicio"));
+    }
 });
 
-$app->post('/crearUsuario',function($request){
+$app->get('/usuario/{id_usuario}', function ($request) {
+    $api_key = $request->gestParam("api_key");
+    session_id($api_key);
+    session_start();
+    if (isset($_SESSION["usuario"]) && $_SESSION["tipo"] == "admin") {
+        echo json_encode(obtener_usuario($request->getAttribute("id_usuario")));
+    } else {
+        session_destroy();
+        echo json_encode(array("no_login" => "No tienes permisos para usar este servicio"));
+    }
+});
 
-    $datos[]=$request->getParam('nombre'); 
-    $datos[]=$request->getParam('usuario');    
-    $datos[]=$request->getParam('clave');
-    $datos[]=$request->getParam('email');
+$app->post('/crearUsuario', function ($request) {
+    $api_key = $request->gestParam("api_key");
+    session_id($api_key);
+    session_start();
+    if (isset($_SESSION["usuario"]) && $_SESSION["tipo"] == "admin") {
+        $datos[] = $request->getParam('nombre');
+        $datos[] = $request->getParam('usuario');
+        $datos[] = $request->getParam('clave');
+        $datos[] = $request->getParam('email');
 
-    echo json_encode(insertar_usuario($datos));
+        echo json_encode(insertar_usuario($datos));
+    } else {
+        session_destroy();
+        echo json_encode(array("no_login" => "No tienes permisos para usar este servicio"));
+    }
 });
 
 
-$app->post('/login',function($request){
+$app->post('/login', function ($request) {
 
-    $usuario=$request->getParam('usuario');    
-    $clave=$request->getParam('clave');
+    $usuario = $request->getParam('usuario');
+    $clave = $request->getParam('clave');
 
-    echo json_encode(login($usuario,$clave));
+    echo json_encode(login($usuario, $clave));
+});
+$app->post('/logueado', function ($request) {
+    $api_key = $request->gestParam("api_key");
+    session_id($api_key);
+    session_start();
+    if (isset($_SESSION["usuario"])) {
+        echo json_encode(logueado($_SESSION["usuario"], $_SESSION["clave"]));
+    } else {
+        session_destroy();
+        echo json_encode(array("no_login" => "No logueado"));
+    }
 });
 
 
-$app->put('/actualizarUsuario/{id_usuario}',function($request){
+$app->put('/actualizarUsuario/{id_usuario}', function ($request) {
+    $api_key = $request->gestParam("api_key");
+    session_id($api_key);
+    session_start();
+    if (isset($_SESSION["usuario"]) && $_SESSION["tipo"] == "admin") {
+        $datos[] = $request->getParam('nombre');
+        $datos[] = $request->getParam('usuario');
+        $datos[] = $request->getParam('clave');
+        $datos[] = $request->getParam('email');
+        $datos[] = $request->getAttribute('id_usuario');
 
-    $datos[]=$request->getParam('nombre'); 
-    $datos[]=$request->getParam('usuario');    
-    $datos[]=$request->getParam('clave');
-    $datos[]=$request->getParam('email');
-    $datos[]=$request->getAttribute('id_usuario');
-
-    echo json_encode(actualizar_usuario($datos));
+        echo json_encode(actualizar_usuario($datos));
+    } else {
+        session_destroy();
+        echo json_encode(array("no_login" => "No tienes permisos para usar este servicio"));
+    }
 });
 
-$app->put('/actualizarUsuarioSinClave/{id_usuario}',function($request){
+$app->put('/actualizarUsuarioSinClave/{id_usuario}', function ($request) {
+    $api_key = $request->gestParam("api_key");
+    session_id($api_key);
+    session_start();
+    if (isset($_SESSION["usuario"]) && $_SESSION["tipo"] == "admin") {
+        $datos[] = $request->getParam('nombre');
+        $datos[] = $request->getParam('usuario');
+        $datos[] = $request->getParam('email');
+        $datos[] = $request->getAttribute('id_usuario');
 
-    $datos[]=$request->getParam('nombre'); 
-    $datos[]=$request->getParam('usuario');    
-    $datos[]=$request->getParam('email');
-    $datos[]=$request->getAttribute('id_usuario');
-
-    echo json_encode(actualizar_usuario_sin_clave($datos));
+        echo json_encode(actualizar_usuario_sin_clave($datos));
+    } else {
+        session_destroy();
+        echo json_encode(array("no_login" => "No tienes permisos para usar este servicio"));
+    }
 });
 
-$app->delete('/borrarUsuario/{id_usuario}',function($request){
-
-    echo json_encode(borrar_usuario($request->getAttribute('id_usuario')));
+$app->delete('/borrarUsuario/{id_usuario}', function ($request) {
+    $api_key = $request->gestParam("api_key");
+    session_id($api_key);
+    session_start();
+    if (isset($_SESSION["usuario"]) && $_SESSION["tipo"] == "admin") {
+        echo json_encode(borrar_usuario($request->getAttribute('id_usuario')));
+    } else {
+        session_destroy();
+        echo json_encode(array("no_login" => "No tienes permisos para usar este servicio"));
+    }
 });
 
-$app->get('/repetido/{tabla}/{columna}/{valor}',function($request){
-
-    echo json_encode(repetido($request->getAttribute('tabla'),$request->getAttribute('columna'),$request->getAttribute('valor')));
+$app->get('/repetido/{tabla}/{columna}/{valor}', function ($request) {
+    $api_key = $request->gestParam("api_key");
+    session_id($api_key);
+    session_start();
+    if (isset($_SESSION["usuario"]) && $_SESSION["tipo"] == "admin") {
+        echo json_encode(repetido($request->getAttribute('tabla'), $request->getAttribute('columna'), $request->getAttribute('valor')));
+    } else {
+        session_destroy();
+        echo json_encode(array("no_login" => "No tienes permisos para usar este servicio"));
+    }
 });
 
-$app->get('/repetido/{tabla}/{columna}/{valor}/{columna_id}/{valor_id}',function($request){
-
-    echo json_encode(repetido($request->getAttribute('tabla'),$request->getAttribute('columna'),$request->getAttribute('valor'),$request->getAttribute('columna_id'),$request->getAttribute('valor_id')));
+$app->get('/repetido/{tabla}/{columna}/{valor}/{columna_id}/{valor_id}', function ($request) {
+    $api_key = $request->gestParam("api_key");
+    session_id($api_key);
+    session_start();
+    if (isset($_SESSION["usuario"]) && $_SESSION["tipo"] == "admin") {
+        echo json_encode(repetido($request->getAttribute('tabla'), $request->getAttribute('columna'), $request->getAttribute('valor'), $request->getAttribute('columna_id'), $request->getAttribute('valor_id')));
+    } else {
+        session_destroy();
+        echo json_encode(array("no_login" => "No tienes permisos para usar este servicio"));
+    }
+});
+$app->post('/salir', function ($request) {
+    session_id($request->getParam('api_key'));
+    session_start();
+    session_destroy();
+    echo json_encode(array("logout" => "Close sesion"));
 });
 
 
 $app->run();
-
-?>
